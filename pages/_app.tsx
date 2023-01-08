cc2bd3762c3e6488923e1ae62cf892525a9e2c08
sinchang <sinchangwen@gmail.com>
2023-01-08 17:56:26 +0800

    Add @vercel/analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import '../styles/globals.css'
 import '@geist-ui/style'
 import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
 
 export default MyApp
