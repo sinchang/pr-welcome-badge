6db5452d23313886876eb45ae2bd6721214823ce
sinchang <sinchangwen@gmail.com>
2023-02-18 21:44:05 +0800

    Correct next/script usage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,19 @@
 import '../styles/globals.css'
 import '@geist-ui/style'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        data-website-id='ffede72a-c791-46bd-ab88-1c5f52d6dfa6'
+        src='https://umami-production-af5b.up.railway.app/umami.js'
+        strategy='afterInteractive'
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
